4c417d6eab20bce4c27478fd56e3548dd7e5b0ed
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2021-03-04 17:26:28 -0500

    Error: listen EADDRINUSE: address already in use 3001 - solved

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const uuid = require("uuid");
+const colors = require("colors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 const myDatabase = require("./configs/db.config");
 const myCors = require("./configs/cors.config");
 const myPassport = require("./configs/passport");
@@ -38,4 +39,8 @@ app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
 app.use("/transactions", require("./routes/transactions"));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV.yellow} mode on port ${PORT.yellow}`
+  )
+);
